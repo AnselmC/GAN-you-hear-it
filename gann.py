64ef2e59f2eceaafb746fd972a75cadfc1c4c550
Anselm Coogan <anselm.coogan@icloud.com>
2020-02-19 17:59:37 +0100

    enhancements to networks (avg pooling for D, config dict for Conv G)

--- gann.py
+++ gann.py
@@ -4,11 +4,38 @@ from torch.autograd import Variable
 
 
 def compute_conv_output_dim(input_h, input_w, kernel_size, padding, stride):
-    output_h = int(((input_h - kernel_size[0] + 2 * padding[0]) / stride[0]) +
-                   1)
-    output_w = int(((input_w - kernel_size[1] + 2 * padding[1]) / stride[1]) +
-                   1)
-    return output_h, output_w
+    kernel_h, kernel_w = kernel_size if isinstance(
+        kernel_size, tuple) else (kernel_size, kernel_size)
+    stride_h, stride_w = stride if isinstance(stride, tuple) else (stride,
+                                                                   stride)
+    padding_h, padding_w = padding if isinstance(padding, tuple) else (padding,
+                                                                       padding)
+    output_h = int(((input_h - kernel_h + 2 * padding_h) / stride_h) + 1)
+    output_w = int(((input_w - kernel_w + 2 * padding_w) / stride_w) + 1)
+    return int(output_h), int(output_w)
+
+
+def compute_deconv_output_dim(input_h, input_w, kernel_size, dilation, stride,
+                              padding, out_padding):
+    kernel_h, kernel_w = kernel_size if isinstance(
+        kernel_size, tuple) else (kernel_size, kernel_size)
+    dilation_h, dilation_w = dilation if isinstance(
+        dilation, tuple) else (dilation, dilation)
+    stride_h, stride_w = stride if isinstance(stride, tuple) else (stride,
+                                                                   stride)
+    padding_h, padding_w = padding if isinstance(padding, tuple) else (padding,
+                                                                       padding)
+    out_padding_h, out_padding_w = out_padding if isinstance(
+        out_padding, tuple) else (out_padding, out_padding)
+    output_h = int((input_h - 1) - 2 * padding_h + dilation_h *
+                   (kernel_h - 1) + out_padding_h + 1)
+    output_w = int((input_w - 1) - 2 * padding_h + dilation_w *
+                   (kernel_w - 1) + out_padding_w + 1)
+    return int(output_h), int(output_w)
+
+
+def compute_pool_output_dim(input_h, input_w, kernel_size):
+    return int(input_h / kernel_size[0]), int(input_w / kernel_size[1])
 
 
 class CustomModel(nn.Module):
@@ -30,12 +57,11 @@ class LinearGenerator(CustomModel):
                  output_h,
                  output_w,
                  model=None,
-                 entropy_size=10,
+                 entropy_size=128,
                  lr=0.0001):
         super(LinearGenerator, self).__init__(device)
         self.output_h = output_h
         self.output_w = output_w
-        self.entropy_size = entropy_size
         self.entropy_size = entropy_size
 
         def ganlayer(input_dim, output_dim, dropout=True):
@@ -79,54 +105,88 @@ class ConvolutionalGenerator(CustomModel):
     :param entropy_size: the size of the entropy noise (default: 1024)
     :param num_beats: the number of time steps in the Short-Time Fourier representation of the signal
     """
+    CONFIG = {
+        "num_layers": 9,
+        "in_channels": [-1, 64, 48, 32, 24, 16, 12, 8, 4],
+        "out_channels": [64, 48, 32, 24, 16, 12, 8, 4, 2],
+        "kernels": [3, 3, 3, 3, 3, 3, 3, 3, 3],
+        "strides": [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        "paddings": [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        "out_paddings": [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        "dilations": [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        "dropout": [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+        "relu_params": [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]
+    }
+
+    @staticmethod
+    def conv_layer(in_channels, out_channels, kernel_size, stride, padding,
+                   dropout, relu_param):
+        layer = [
+            nn.ConvTranspose2d(in_channels=in_channels,
+                               out_channels=out_channels,
+                               kernel_size=kernel_size,
+                               padding=padding,
+                               stride=stride),
+            nn.BatchNorm2d(out_channels),
+            nn.Dropout(dropout),
+            nn.LeakyReLU(relu_param, inplace=True)
+        ]
+        return layer
+
+    @staticmethod
+    def build_model(config, entropy_size, final_output_h, final_output_w):
+        num_layers = config["num_layers"]
+        config["in_channels"][0] = entropy_size
+        model = []
+        output_h = 1
+        output_w = 1
+        output_c = config["out_channels"][-1]
+        print("GENERATOR DIMENSIONS")
+        for i in range(num_layers):
+            in_channels = config["in_channels"][i]
+            out_channels = config["out_channels"][i]
+            print(f"{output_h}x{output_w}x{in_channels}")
+            kernel_size = config["kernels"][i]
+            stride = config["strides"][i]
+            padding = config["paddings"][i]
+            dropout = config["dropout"][i]
+            dilation = config["dilations"][i]
+            out_padding = config["out_paddings"][i]
+            relu_param = config["relu_params"][i]
+            layer = ConvolutionalGenerator.conv_layer(in_channels,
+                                                      out_channels,
+                                                      kernel_size, stride,
+                                                      padding, dropout,
+                                                      relu_param)
+            model += layer
+            output_h, output_w = compute_deconv_output_dim(
+                output_h, output_w, kernel_size, dilation, stride, padding, out_padding)
+        print(f"{output_h}x{output_w}x{out_channels}")
+        output_size = output_h * output_w * output_c
+        model = nn.Sequential(
+            *model, nn.Flatten(),
+            nn.Linear(output_size, 2 * final_output_h * final_output_w),
+            nn.Tanh())
+        print(f"{final_output_h}x{final_output_w}x2")
+        print(model)
+        return model
+
     def __init__(self,
                  device,
                  output_h,
                  output_w,
                  model=None,
-                 entropy_size=10,
-                 lr=0.0001):
+                 entropy_size=128,
+                 lr=0.0001,
+                 config=CONFIG):
         super(ConvolutionalGenerator, self).__init__(device)
         self.output_h = output_h
         self.output_w = output_w
         self.entropy_size = entropy_size
 
-        self.model = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=self.entropy_size,
-                               out_channels=8,
-                               kernel_size=2,
-                               padding=0,
-                               stride=1),
-            nn.BatchNorm2d(8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.ConvTranspose2d(in_channels=8,
-                               out_channels=6,
-                               kernel_size=2,
-                               padding=0,
-                               stride=1),
-            nn.BatchNorm2d(6),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(0.5),
-            nn.ConvTranspose2d(in_channels=6,
-                               out_channels=4,
-                               kernel_size=2,
-                               padding=0,
-                               stride=1),
-            nn.BatchNorm2d(4),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(0.5),
-            nn.ConvTranspose2d(in_channels=4,
-                               out_channels=2,
-                               kernel_size=2,
-                               padding=0,
-                               stride=1),
-            nn.BatchNorm2d(2),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(0.5),
-            nn.Flatten(),
-            nn.Linear(50, 2 * self.output_w * self.output_h),
-            nn.Tanh()  # frequency amplitudes are normalized
-        )
+        self.model = ConvolutionalGenerator.build_model(
+            config, entropy_size, output_h, output_w)
+
         self.optim = optim.Adam(self.parameters(), lr=lr)
         self.loss = nn.BCELoss()
         self.load(model)
@@ -156,17 +216,18 @@ class Discriminator(CustomModel):
 
     CONFIG = {
         "num_layers": 5,
-        "in_channels": [2, 4, 6, 8, 10],
-        "out_channels": [4, 6, 8, 10, 12],
-        "kernels": [(4, 13), (4, 10), (4, 4), (4, 4), (4, 4)],
-        "strides": [(1, 4), (1, 2), (1, 2), (1, 2), (1, 2)],
+        "in_channels": [2, 8, 16, 32, 64],
+        "out_channels": [8, 16, 32, 64, 128],
+        "kernels": [(4, 13), (4, 10), (3, 3), (3, 3), (3, 3)],
+        "strides": [(1, 4), (1, 2), (1, 1), (1, 1), (1, 1)],
         "paddings": [(2, 0), (2, 0), (2, 0), (2, 0), (2, 0)],
+        "pooling": [(1, 2), (1, 2), (1, 2), (2, 2), (2, 2)],
         "relu_params": [0.2, 0.2, 0.2, 0.2, 0.2]
     }
 
     @staticmethod
     def conv_layer(in_channels, out_channels, kernel_size, stride, padding,
-                   relu_param):
+                   pooling, relu_param):
         layer = [
             nn.Conv2d(in_channels=in_channels,
                       out_channels=out_channels,
@@ -175,6 +236,8 @@ class Discriminator(CustomModel):
                       padding=padding)
         ]
         layer.append(nn.BatchNorm2d(out_channels))
+        if pooling != (0, 0):
+            layer.append(nn.AvgPool2d(pooling))
         layer.append(nn.LeakyReLU(relu_param, inplace=True))
         return layer
 
@@ -185,19 +248,26 @@ class Discriminator(CustomModel):
         output_h = input_h
         output_w = input_w
         output_c = config["out_channels"][-1]
+        print("DISCRIMINATOR DIMENSIONS")
         for i in range(num_layers):
             in_channels = config["in_channels"][i]
             out_channels = config["out_channels"][i]
+            print(f"{output_h}x{output_w}x{in_channels}")
             kernel_size = config["kernels"][i]
             stride = config["strides"][i]
             padding = config["paddings"][i]
+            pooling = config["pooling"][i]
             relu_param = config["relu_params"][i]
             layer = Discriminator.conv_layer(in_channels, out_channels,
                                              kernel_size, stride, padding,
-                                             relu_param)
+                                             pooling, relu_param)
             model += layer
             output_h, output_w = compute_conv_output_dim(
                 output_h, output_w, kernel_size, padding, stride)
+            if pooling != (0, 0):
+                output_h, output_w = compute_pool_output_dim(
+                    output_h, output_w, pooling)
+        print(f"{output_h}x{output_w}x{out_channels}")
         output_size = output_h * output_w * output_c
         return nn.Sequential(*model), output_size
 
